3be0b1f218a87161e46d2320b42c083be2f40dc6
ramosovov <umur.kilickaya@gmail.com>
2023-02-10 09:18:27 +0300

    Assets and Routes

---
+++ public/js/otel.js
@@ -0,0 +1,14 @@
+///////////////////////////////////////POP UP///////////////////////////////////////
+function popups(name,open){
+    if(open){
+      $("." + name + "-main").addClass('model-open');
+    }
+   else{
+    $("." + name + "-main").removeClass('model-open');
+   }
+  
+  }
+
+
+
+  
