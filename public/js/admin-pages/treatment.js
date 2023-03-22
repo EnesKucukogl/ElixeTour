2494a66b1756b212ece9002065abdefb9a72c109
EnesKucukogl <eneskucukogl35@gmail.com>
2023-03-22 16:12:47 +0300

    treatment small fixes

--- public/js/admin-pages/treatment.js
+++ public/js/admin-pages/treatment.js
@@ -673,12 +673,39 @@ $(document).ready(function () {
                 {
                     dataField: "translation_description",
                     label: {
-                        text: 'Tedavi Açıklama (' + data.symbol+ ')'
+                        text: 'Tedavi Açıklaması (' + data.symbol + ')'
                     },
-                    editorType: "dxTextArea",
+                    editorType: "dxHtmlEditor",
                     editorOptions: {
-                        height: 100
-                    }
+                        height: 300,
+                        toolbar: {
+                            items: [
+                                'undo', 'redo', 'separator',
+                                {
+                                    name: 'size',
+                                    acceptedValues: ['8pt', '10pt', '12pt', '14pt', '18pt', '24pt', '36pt'],
+                                },
+                                {
+                                    name: 'font',
+                                    acceptedValues: ['Arial', 'Courier New', 'Georgia', 'Impact', 'Lucida Console', 'Tahoma', 'Times New Roman', 'Verdana'],
+                                },
+                                'separator', 'bold', 'italic', 'strike', 'underline', 'separator',
+                                'alignLeft', 'alignCenter', 'alignRight', 'alignJustify', 'separator',
+                                'orderedList', 'bulletList', 'separator',
+                                {
+                                    name: 'header',
+                                    acceptedValues: [false, 1, 2, 3, 4, 5],
+                                }
+                            ],
+                        },
+                        mediaResizing: {
+                            enabled: true,
+                        }
+                    },
+                    validationRules: [{
+                        type: "required",
+                        message: "Tedavi Açıklaması boş geçilemez !"
+                    }]
                 },
             ]
         }
